253e65dab2c96fefa7f26d985d4e521c7acf46f8
J Litt <jdlitton2594@gmail.com>
2022-10-11 09:55:15 -0500

    did most of the back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ApolloProvider} from '@apollo/client';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
@@ -6,16 +7,18 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -30,6 +30,36 @@ const resolvers = {
 
             const token = signToken(user);
             return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const book = await Book.create({ ...args, username: context.user.username});
+
+                await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push:  { books: book._id } },
+                    { new: true}
+                );
+
+                return book;
+            }
+
+            throw new AuthenticationError('You need to be logged in')
+        },
+        deleteBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: {books: book._id } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in')
         }
     }
-}
+};
+
+module.exports = resolvers;
